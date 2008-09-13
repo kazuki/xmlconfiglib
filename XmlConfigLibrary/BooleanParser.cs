03c78d052ea88ac040c465a8328a377240d13bf7
Kazuki Oikawa <k@oikw.org>
2008-09-13 02:52:35 +0000

    * BooleanParserを追加 (ファイル追加し忘れ…)

---
+++ XmlConfigLibrary/BooleanParser.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Xml;
+
+namespace XmlConfigLibrary
+{
+	public class BooleanParser : IParser<bool>
+	{
+		static BooleanParser _instance = new BooleanParser ();
+		BooleanParser() {}
+		public static BooleanParser Instance {
+			get { return _instance; }
+		}
+
+		public bool TryParse (XmlConfigNode node, out bool value)
+		{
+			value = true;
+			if (node.NodeValue == null || !bool.TryParse (node.NodeValue, out value))
+				return false;
+			return true;
+		}
+
+		public void Write (bool value, XmlWriter writer)
+		{
+			writer.WriteString (value.ToString ());
+		}
+	}
+}
